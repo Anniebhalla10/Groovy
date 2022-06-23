b5868ec3b0071a61f721f7b084995f0d40c1eb9d
Anniebhalla10 <anniebhalla2001@gmail.com>
2022-06-23 19:19:25 +0530

    login route

--- src/App.js
+++ src/App.js
@@ -2,29 +2,16 @@ import { Route, Routes, useNavigate} from "react-router-dom";
 import './App.scss';
 import { useEffect, useState } from 'react';
 import Login from './components/Login';
+import Home from "./components/Home";
 
 function App() {
-  // const [ isLoggedIn, setIsLoggedIn]= useState(false);
-  // const navigate= useNavigate();
-
-  // const checkLogin=()=>{
-  //   if(!isLoggedIn){
-  //     navigate('./login', {replace: true});
-  //   }
-  // }
-
-  // useEffect(()=>{
-  //   checkLogin();
-  // },[]);
 
   return (
   <>
-  {/* <Routes> */}
-      {/* <Route path="/" element={}/> */}
-       {/* <Route path='./login' element={<Login/>}/>
-  </Routes>  */}
-  <Login/>
-
+  <Routes> 
+  <Route path="/" element={<Login/>}/>
+  <Route path="/home" element={<Home />} />
+  </Routes> 
   </>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,78 +1,103 @@
-import React from 'react'
-import { Form, Input, Checkbox, Button } from "antd";
+import React, { useState } from "react";
+import { Form, Input, Checkbox, Button, message } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
 
+  const defaultCredential = {
+    username: "",
+    password: "",
+  };
+  const [credential, setCredential] = useState({ ...defaultCredential });
+
+  const submitHandler = () => {
+    if (
+      credential.username === "annie" ||
+      credential.username === "harsh" ||
+      credential.password === "groovy1234"
+    ) {
+      navigate("/home", { replace: true });
+    } else {
+      message.error("Wrong credentials");
+    }
+  };
+
+  console.log(credential);
   return (
     <>
-        <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-    //   onFinish={onFinish}
-    //   onFinishFailed={onFinishFailed}
-      autoComplete="off"
-      className='login_form'
-    >
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        //   onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        className="login_form"
+      >
         <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input
+            onChange={(e) =>
+              setCredential({ ...credential, username: e.target.value })
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password
+            onChange={(e) =>
+              setCredential({ ...credential, password: e.target.value })
+            }
+          />
+        </Form.Item>
 
-        
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-
-    </Form>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" onClick={submitHandler}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </>
-  )
-}
+  );
+};
 
 export default Login;
