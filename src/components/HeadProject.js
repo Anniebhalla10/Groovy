e70c8827ec631fa4e34d2b71a12cf54c18cea9c7
Anniebhalla10 <anniebhalla2001@gmail.com>
2022-06-27 09:31:30 +0530

    headerbar

--- src/components/HeadProject.js
+++ src/components/HeadProject.js
@@ -1,22 +1,50 @@
 import { Button } from "antd";
 import { AiFillHeart, AiFillHome, AiFillSetting } from "react-icons/ai";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const HeadProject = () => {
-    const navigate= useNavigate();
+  const navigate = useNavigate();
+  const [greet, setGreet] = useState("");
+
+  const getGreet = () => {
+    var time = new Date().getHours();
+    if (time >= 4 && time < 12) {
+      setGreet("Morning");
+    } else if (time >= 12 && time < 17) {
+      setGreet("Afternoon");
+    } else if (time >= 17 || time < 4) {
+      setGreet("Night");
+    }
+  };
+
+  useEffect(() => {
+    getGreet();
+    setInterval(() => {
+      getGreet();
+    }, 30000);
+  }, []);
+
   return (
     <>
-    <div className="headproject">
-      <div className="sidebar">
-        <Button icon={<AiFillHome />} className="sidebar-btn" onClick={()=>navigate('/home')}/>
-        <Button icon={<AiFillHeart />} className="sidebar-btn" onClick={()=> navigate('/home/favourites')}/>
-        <Button icon={<AiFillSetting/>} className="sidebar-btn" />
+      <div className="headproject">
+        <div className="sidebar">
+          <Button
+            icon={<AiFillHome />}
+            className="sidebar-btn"
+            onClick={() => navigate("/home")}
+          />
+          <Button
+            icon={<AiFillHeart />}
+            className="sidebar-btn"
+            onClick={() => navigate("/home/favourites")}
+          />
+          <Button icon={<AiFillSetting />} className="sidebar-btn" />
+        </div>
       </div>
 
       <div className="headbar">
-
-      </div>
+        <div className="headbar-greeting">Good {greet}</div>
       </div>
     </>
   );
