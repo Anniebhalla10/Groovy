26b04db7e7da6a3c01d75e729cc7495eae2c2bb3
HY <62005923+shad0w-cat@users.noreply.github.com>
2022-06-29 22:29:23 +0530

    Music db added and music files name changed

---
+++ src/db/music.js
@@ -0,0 +1,10 @@
+export const musicDb = [{
+    id: "",
+    title: "Name",
+    artist: "names",
+    albumName: "",
+    genre: "",
+    year: "",
+    albumArtFormat: "",
+    albumArtData: ""
+}]
